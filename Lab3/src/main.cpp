dd19d846ccf1b76830f03e56a4bf262c72277330
Tim Lee <hunjael@smu.edu>
2019-11-12 00:26:43 -0600

    polish code add comment

--- Lab3/src/main.cpp
+++ Lab3/src/main.cpp
@@ -7,7 +7,7 @@
 //#include "tsp.h"
 
 int main () {
-    char* input = "datafiles/positions.txt";
+    char* input = "datafiles/positions.txt";    //When testing against private list of nodes, change this value.
     tsp_interface* k = new tsp_dp(input);
     k -> run_tsp();
     k -> display();
